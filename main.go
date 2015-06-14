5363afd32e8ab162fbcbd8ee04afbee788b9ca03
azer <azer.xu@gmail.com>
2015-06-14 13:25:12 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":54321", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
 }
